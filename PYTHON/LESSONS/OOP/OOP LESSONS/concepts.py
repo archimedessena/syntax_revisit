559e19d5be574d854064634703572f46fe161b93
Archimedes Senadju <archimedessena@gmail.com>
2024-11-02 17:14:22 +0000

    turtle_example_illutrating_objects

--- PYTHON/LESSONS/OOP/OOP LESSONS/concepts.py
+++ PYTHON/LESSONS/OOP/OOP LESSONS/concepts.py
@@ -13,3 +13,40 @@
 # Methods are functions in a class 
 # Property is a variable in a class
 # A Property and Method must be linked to an object to work
+
+# Turtle Example
+
+from turtle import  * 
+
+t1 = Turtle()
+#t2 = Turtle()
+#t1.color("orange")
+#t2.color("purple")
+#
+#t1.forward(50)
+#t1.left(90)
+#
+#t2.left(90)
+#t2.forward(50)
+
+#done()
+
+t1.color("orange")
+t1.width(5)
+
+t1.begin_fill()
+for i in range(5):
+    t1.forward(150)
+    t1.left(144)
+t1.end_fill()
+
+
+done()
+
+
+
+
+
+
+
+
