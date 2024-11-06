850056a2192f1b1327f7d78c91181ec4984c618d
Archimedes Senadju <archimedessena@gmail.com>
2024-11-06 08:29:31 +0000

    online_management_system

---
+++ PYTHON/LESSONS/OOP/OOP LESSONS/ex9_onlinecourse.py
@@ -0,0 +1,4 @@
+# Title: Online Course Management System
+
+
+# Description: The code respresents an online course management systme that helps keep track of online courses, students, and their grades. it allows you to do things like enroll students, see course details, and calculate average grades.
--- PYTHON/LESSONS/OOP/OOP LESSONS/test.py
+++ PYTHON/LESSONS/OOP/OOP LESSONS/test.py
@@ -1,35 +1,49 @@
 class App():
-    def __init__(self,  users, storages, username):
-        self.username = username
+    def __init__(self, users, username, storages):
         self.users = users
+        self.username = username
         self.storages = storages
         
     
     def login(self):
-        if self.username == "sena" and self.users >=1:
-            print("Hi,", self.username)
+        if self.username == "Archimedes" and self.users <= 10:
+            print("Access granted.")
+            print("Greetings,", self.username)
+            
+        elif self.username == "Sena" and self.users <= 10:
+            print("Access granted.")
+            print("Greetings,", self.username)
+        
         else:
-            print("Login denied.")
-            
-        
+            print("Access denied.")
+           
+    
+ 
     def increase_capacity(self, number):
-        self.storages += number
-        print(self.storages)
-    
-    
-    
-    
-                        
-sena = App(500, 99, "owner")
-sena.login()
-sena.increase_capacity(180)
-
-#sena = App(800, 899, "user")
-#sena.login()
-
+        if self.storages >= 1500 and number >= 1500:
+            self.storages += number
+            print(self.storages, "is out of range")
             
+        elif self.storages >= 1500 and number <= 1500:
+            self.storages += number
+            print("Your updated storage is:", self.storages)
+        
+        else:
+            print("You have not added enough storage.")
             
         
-        
-        
+    #def check_upgrade(self):
+    #    if self.users >= self.storages:
+    #        upgrade_amt = int(input("Please enter upgrade_amount: "))
+    #        self.storages += upgrade_amt
+    #    
+    #    else:
+    #        print("You will still have:", str(self.storages - self.users), "spaces open!")
+            
+            
+t = App(10, "Sena", 1500)   
+
+t.login()    
+t.increase_capacity(1400)    
+#t.check_upgrade()   
         
