9ce03c69e2bf02524856c597e5981652a1b2b4e9
Archimedes Senadju <archimedessena@gmail.com>
2024-11-19 20:16:56 +0000

    bugfixed

--- PYTHON/LESSONS/OOP/OOP LESSONS/ex9_onlinecourse.py
+++ PYTHON/LESSONS/OOP/OOP LESSONS/ex9_onlinecourse.py
@@ -15,19 +15,22 @@ class OnlineCourse:
     def course_details(self):
         print(f"Course: {self.course}")
         print(f"Instructor Name:", self.instructor)
-        print(f"Enrolled Students: {', '.join(self.students)}")
-        
+        print(f"Enrolled Students:")  
+        for student in self.students:
+            print(student.name)
+            
     def completed_course(self, name):
-        if name in self.students:
-            self.students.remove(name)
-            print(f"{name} has completed course!")
-        else:
-            print(f"{name} is not enrolled in this course.")
+        for student in self.students:
+            if student.name in name:
+                self.students.remove(student)
+                print(f"{name} has completed course!")
+            else:
+                print(f"{name} is not enrolled in this course.")
                       
     def avg_grade(self, grades):
         total = sum(grades)
         average = total / len(grades)
-        print(f"The average grade is: {average}")
+        print(f"The average grade is: {average,1}")
         
         
 
@@ -42,29 +45,21 @@ course_input = input("Enter a course: ").lower()
 name = input("Enter an Instructor: ").lower()
 student = input("Enter name of student: ").lower()
 
-#course = OnlineCourse(course_input, name) 
-#grades = [90, 85, 92, 78, 80]
-#
-#course.avg_grade(grades)
-#course.enroll_students(student)
-#course.course_details()
-#course.completed_course(name="Emelia")
 
 course_input = input("Enter a course: ").lower() 
 name = input("Enter an instructor: ").lower()
 course = OnlineCourse(course_input, name)
 
 
-
-num_students = int(input("Enter number of students"))
+num_students = int(input("Enter number of students:"))
 
 for _ in range(num_students):
-    student_name = input("Enter student name: ").lower()
+    student_name = input("Enter a student name: ").lower()
     grades = []
     for _ in range(3):
-        grade = int(input("Enter grade: "))
+        grade = int(input("Enter a grade: "))
         grades.append(grade) 
-    student = student(student_name, grades)
+    student = Student(student_name, grades)
     course.enroll_students(student)
     course.avg_grade(student)
 course.course_details()
