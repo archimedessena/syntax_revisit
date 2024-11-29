4ac9990191c9c78e7ce93c4c616d901f13144a00
Archimedes Senadju <archimedessena@gmail.com>
2024-11-29 18:04:11 +0000

    travelplannerupdated

--- PYTHON/LESSONS/OOP/OOP LESSONS/ex11_travel.py
+++ PYTHON/LESSONS/OOP/OOP LESSONS/ex11_travel.py
@@ -3,11 +3,11 @@
 # Description: The provided code represents  a travel planning and cost calculator system. It allows you to input the country you want to travel to, the month of travel, and the type of trip (leisure or business). The system provides information about the trip and calculates the total cost based on flight expenses and additional costs.
 
 #Lets understand the how the code works.
-# 1. Travel planing: When you create a new instance of the "Travel" class, you provide the country you want to visit, the month of travel, and the type of trip (leisure or business). The system stores this information.
+# 1. Travel planning: When you create a new instance of the "Travel" class, you provide the country you want to visit, the month of travel, and the type of trip (leisure or business). The system stores this information.
 
 # 2. Trip Information: The "trip_info()" method provides information about your trip based on the month of travel. if the month falls between October and March, it considers it as a winter trip and displays a message stating the country and trip type (leisure or business). If the month is between April and September, it considers it as a summer trip and displays a similar message. If an invalid month is entered, it displays an error message
 
-#3. Cost Calculation: The "calc_cost(cost)" method calculates the cost the total cost of the trip. It takes the flight cost as input and prompts you to enter any additional cost. The system keeps adding the cost and updates the total price accordingly. It also provides advice based on the total cost, categorizing it as "low budge" for costs below 500, "Enjoy a flight to anywhere" for costs between 500 and 1500, and "Luxury trip" for costs exceeding 1500
+#3. Cost Calculation: The "calc_cost(cost)" method calculates the cost the total cost of the trip. It takes the flight cost as input and prompts you to enter any additional cost. The system keeps adding the cost and updates the total price accordingly. It also provides advice based on the total cost, categorizing it as "low budget" for costs below 500, "Enjoy a flight to anywhere" for costs between 500 and 1500, and "Luxury trip" for costs exceeding 1500
 
 
 class Travel:
@@ -21,7 +21,26 @@ class Travel:
     def trip_info(self):
         if self.month >= 10 and self.month <= 3:
             print(f"You are going to {self.country} in the winter! This is a {self.type} trip")
-
+        elif self.month > 3 and self.month < 10:
+             print(f"You are going to {self.country} in the summer! This is a {self.type} trip")
+        else:
+            print("Invalid Input")
+    
+    
+    def calc_cost(self, cost):
+        costs = []
+        while cost != 0:
+            self.price += cost
+            costs.append(cost)
+            cost = int(input("Enter another cost:"))
+            
+    def advice(self, number):
+        if number < 500:
+            print("You have low budget")
+        elif number >= 500 and number < 1500:
+            print("Take a flight to anywhere.")
+        else:
+            print("Luxury trip.")
 
 
 
